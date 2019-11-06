64329c209c6dcadba72aa80a2d9801e73b68bfa0
Aravind <aravindbe95@gmail.com>
2019-11-06 14:04:39 +0530

    6th tamil medium

---
+++ science/js/data8.js
@@ -0,0 +1,289 @@
+var Botany12Chapter8MCQ = [
+    {
+        question: "",
+        answers:
+        {
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        },
+        correctAnswer: ''
+    },
+    {
+        question: " ",
+        answers:
+        {
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        },
+        correctAnswer: ''
+    },
+    {
+        question: "3. கீழ்க்கண்டவற்றில் எது ஹியூகோ டி விரிஸின் பங்களிப்பு?",
+        answers:
+        {
+            a: 'திடீர் மாற்றத் தேர்வுக் கோட்பாடு',
+            b: 'இயற்கைத் தேர்வுக் கோட்பாடு',
+            c: 'முயன்று பெற்ற பண்பு மரபுப்பண்பாதல் கோட்பாடு',
+            d: 'வளர்கரு பிளாசக் கோட்பாடு'
+        },
+        correctAnswer: ' a'
+    },
+    {
+        question: "4. பறவைகள் மற்றும் வண்ணத்துப்பூச்சிகளின் இறக்கைகள் கீழ்க்கண்ட எதற்கு எடுத்துக்காட்டுகளாகும்.",
+        answers:
+        {
+            a: 'பரவல் முறை தகவமைப்பு',
+            b: 'குவி பரிணாமம்',
+            c: 'விரி பரிணாமம்',
+            d: 'மாறுபாடுகள்'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "5. &lsquo;தொழிற்சாலை மெலானினாக்கம்' என்ற நிகழ்வு கீழ்க்கண்ட எதனை விளக்குகிறது?",
+        answers:
+        {
+            a: ' இயற்கைத் தேர்வு',
+            b: 'தூண்டப்பட்ட திடீர் மாற்றம்',
+            c: 'இனப்பெருக்கத் தனிமைப்படுத்தல்',
+            d: 'புவியியல் தனிமைப்படுத்தல்'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "டார்வினின் குருவிகள் கீழ்க்கண்ட எதற்கு எடுத்துகாட்டுகள் ஆகும்?",
+        answers:
+        {
+            a: 'இணைப்பு உயிரிகள்',
+            b: 'பருவகால வலசைபோதல்',
+            c: 'தகவமைப்பு பரவல்',
+            d: 'ஒட்டுண்ணி வாழ்க்கை முறை'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "7. வளர்கரு பிளாசக் (Germplasm) கோட்பாட்டைக் கூறியவர் யார்?",
+        answers:
+        {
+            a: 'டார்வின்',
+            b: 'ஆகஸ்ட் வீஸ்மேன்',
+            c: 'லாமார்க்',
+            d: 'ஆல்ஃப்ரட் வாலாஸ்'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "8. புதைபடிவங்களின் வயதைத் தீர்மானிக்க உதவுவது?",      
+        answers:
+        {
+            a: 'மின்னணு நுண்ணோக்கி',
+            b: 'புதைபடிவங்களின் எடை',
+            c: 'கார்பன் முறை வயது கண்டறிதல் ',
+            d: 'படிவங்களின் எலும்புகளை ஆராய்தல்'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "9. புதைபடிவங்கள் பொதுவாக எங்கே காணப்படுகிறது?",
+        answers:
+        {
+            a: 'வெப்பப் பாறைகள்',
+            b: 'உருமாறும் பாறைகள்',
+            c: 'எரிமலைப் பாறைகள்',
+            d: 'படிவுப் பாறைகள்'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "10. ஒரு உயிரினத்தின் பரிணாம வரலாறு எவ்வாறு அழைக்கப்படும்?",
+        answers:
+        {
+            a: 'மூதாதைத் தன்மை',
+            b: 'ஆன்ட்டோஜெனி',
+            c: 'பைலோஜெனி (இன் வரலாறு)',
+            d: 'தொல்லுயிரியல்'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "11. ஊர்வன இனத்தின் பொற்காலம்",
+        answers:
+        {
+            a: 'மீசோசோயிக் பெருங்காலம்',
+            b: 'சீனோசோயிக் பெருங்காலம்',
+            c: 'பேலியோசோயிக் பெருங்காலம்',
+            d: 'புரோட்டிரோசோயிக் பெருங்காலம்'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "",
+        answers:
+        {
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        },
+        correctAnswer: '' 
+    },
+    {
+        question: "",
+        answers:
+        {
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        },
+        correctAnswer: '' 
+    },
+    {
+        question: "",
+        answers:
+        {
+            a: '',
+            b: '',
+            c: '',
+            d: ''
+        },
+        correctAnswer: ''
+    }
+];
+var Botany12Chapter8TM  = [
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    }
+         
+]; 
+
+
+
+var Botany12Chapter85M  = [
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    },
+    {
+        question: "",
+        answer:""
+    }
+];
+   
---
+++ science/js/subject.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="js/data1.js"></script>
+<script type="text/javascript" src="js/data2.js"></script>
+<script type="text/javascript" src="js/data3.js"></script>
+<script type="text/javascript" src="js/data4.js"></script>
+<script type="text/javascript" src="js/data5.js"></script>
+<script type="text/javascript" src="js/data6.js"></script>
+<script type="text/javascript" src="js/data7.js"></script>
+<script type="text/javascript" src="js/data8.js"></script>
